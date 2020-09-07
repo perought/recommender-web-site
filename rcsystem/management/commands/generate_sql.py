ed01eda616d4e4da56e0deeb0e1388ab5c16ee3c
perought <52033538+perought@users.noreply.github.com>
2020-09-07 13:52:29 +0300

    update sql script generator for users

--- rcsystem/management/commands/generate_sql.py
+++ rcsystem/management/commands/generate_sql.py
@@ -10,17 +10,18 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         datasets = [
-            ["books_dataset", "bookdatabase"],
-            ["books_ratings", "bookratings"],
-            ["movies_dataset_ascii", "moviedatabase"],
-            ["movies_ratings", "movieratings"]
+            ["books_dataset", "rcsystem_bookdatabase"],
+            ["books_ratings", "rcsystem_bookratings"],
+            ["movies_dataset_ascii", "rcsystem_moviedatabase"],
+            ["movies_ratings", "rcsystem_movieratings"],
+            ["users_script", "auth_user"]
         ]
         for dataset in datasets:
             with open('rcsystem/static/' + dataset[0] + '.csv', 'r') as f:
                 csv_file = csv.reader(f)
                 header = next(csv_file)
                 headers = map((lambda x: '`' + x + '`'), header)
-                insert = 'INSERT INTO rcsystem_' + dataset[1] + '(' + ", ".join(headers) + ") VALUES "
+                insert = 'INSERT INTO ' + dataset[1] + '(' + ", ".join(headers) + ") VALUES "
                 sql = []
                 for row in csv_file:
                     values = map((lambda x: '"' + x + '"'), row)
